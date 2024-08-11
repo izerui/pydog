1b82b2b76f65b074f3264f95cb5659ac82226f7a
serv <liuyuhua69@gmail.com>
2024-08-11 11:24:53 +0800

    网狗盘

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 docker build -f Dockerfile -t pydog:1.0 .
-docker tag pydog:1.0 harbor.yj2025.com/library/pydog:1.0
-docker push harbor.yj2025.com/library/pydog:1.0
+docker tag pydog:1.0 izerui/pydog:1.0
+docker push izerui/pydog:1.0
